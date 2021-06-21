d6ba2637680bbdaab43f79c1692a17a9f1acaef4
Flynn <flynnwebdev@outlook.com>
2021-06-21 12:32:40 +1000

    Completed lesson

--- file1.js
+++ file1.js
@@ -1,1 +1,13 @@
-console.log(global);
+const unchangeableNumber = 1234
+
+function add(x, y) {
+    return x + y
+}
+
+function subtract(x, y) {
+    return x - y
+}
+
+export { add, subtract, unchangeableNumber }
+
+export default 42
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-var global = "this is a global var";
+import { add } from './file1.js'
+import _ from './node_modules/lodash-es/lodash.default.js'
+// console.log(Foo, add)
 
-console.log(global);
+// console.log(add(5, 4))
+// console.log(Foo.subtract(5, 4))
+// console.log(Foo.unchangeableNumber)
+console.log(_.random(10))
